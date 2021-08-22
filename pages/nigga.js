ef2ad6671b5489de4eed581816fd39984dbb1917
linha-de-sintra <buedanegro@gmail.com>
2021-08-22 22:28:26 +0100

    nigga v1.1

--- pages/nigga.js
+++ pages/nigga.js
@@ -4,11 +4,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div> <audio controls autoplay>
-    <source src="./hino2.0.m4a" type="audio/m4a">
-    Your browser does not support the audio element.
-  </source>
-  </audio>
+    <div> 
     <Image src="/bandeira_3.png" alt="bandeira da tropa" width={3840} height={2160} />
 </div>
 
