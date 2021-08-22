ab052bc18741bb54c5886eb21f6b0ab2e7dd1a8c
linha-de-sintra <buedanegro@gmail.com>
2021-08-22 22:45:43 +0100

    nigga v1.2 (hino)

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-function play() {
-  var audio = new Audio('./hino2.0.m4a');
-  audio.play();
-}
+
 export default function Home() {
   return (
-    <div> <figure>
-    <figcaption>A Portuguesa - Hino Nacional Português</figcaption>
+    <div> 
+    <Image src="/bandeira_3.png" alt="bandeira da tropa" width={3840} height={2160} />
+    <figure>
     <audio
         autoPlay
+        controls
         loop
         src="./hino2.0.m4a">
             Your browser does not support the
             <code>audio</code> element.
     </audio>
 </figure>
-    <Image src="/bandeira_3.png" alt="bandeira da tropa" width={3840} height={2160} />
 </div>
 
   )
